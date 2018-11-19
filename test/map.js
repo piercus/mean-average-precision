e0365a75fdc951ca6dda6e5d9e88e445cabb7c8a
Tania.Pash <t.pashnyuk@gmail.com>
2018-11-19 17:15:32 +0200

    ref: add test img1 !== img2 & l1=== l2

--- test/map.js
+++ test/map.js
@@ -78,7 +78,7 @@ test('test case: same filename and label for prediction and groundTruth', t => {
 
 	t.true(res > 0);
 	t.true(res <= 1);
-})
+});
 
 test('test case: same filename and different labels for prediction and groundTruth', t => {
 	const groundTruths = [{
@@ -105,7 +105,7 @@ test('test case: same filename and different labels for prediction and groundTru
 	});
 
 	t.is(res, 0);
-})
+});
 
 test('test case: filename and label are different between prediction and groundTruth', t => {
 	const groundTruths = [{
@@ -132,5 +132,32 @@ test('test case: filename and label are different between prediction and groundT
 	});
 
 	t.is(res, 0);
-})
+});
+
+test('test case: filename and label are same for prediction and groundTruth', t => {
+	const groundTruths = [{
+		filename: 'image1.jpg',
+		label: '4_whi_bla',
+		left: 22,
+		top: 34,
+		right: 231,
+		bottom: 78
+	}];
+
+	const predictions = [{
+		filename: 'image2.jpg',
+		confidence: 1,
+		label: '4_whi_bla',
+		left: 25,
+		top: 38,
+		right: 201,
+		bottom: 90
+	}];
+	const res = mAP({
+		groundTruths,
+		predictions
+	});
+
+	t.is(res, 0);
+});
 
