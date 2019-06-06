2f043980cb58fc9ac70878e5589d1645291f09b8
Colle <piercus@users.noreply.github.com>
2019-06-06 10:40:58 +0200

    feat: expose getMatchPredictions

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const mAP = require('./lib/map.js');
 
 mAP.listMisclassified = require('./lib/list-misclassified');
 mAP.iou = require('./lib/iou');
+mAP.getMatchPredictions = require('./lib/get-match-predictions');
 
 module.exports = mAP;
