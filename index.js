e73a72846cfaa8af92d81ae549c835c899720c58
Pierre Colle <piercus@gmail.com>
2021-02-16 16:03:34 +0100

    fix: expose nonMaxSuppression

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const mAP = require('./lib/map.js');
 mAP.listMisclassified = require('./lib/list-misclassified');
 mAP.iou = require('./lib/iou');
 mAP.getMatchPredictions = require('./lib/get-match-predictions');
+mAP.nonMaxSuppression = require('./lib/non-max-suppression');
 
 module.exports = mAP;
