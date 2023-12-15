2da9d6470d0ec80a3b263978826d7336296aeb15
Pierre Colle <piercus@gmail.com>
2023-12-15 12:13:17 +0100

    feat: reducer

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ mAP.iou = require('./lib/iou');
 mAP.getMatchPredictions = require('./lib/get-match-predictions');
 mAP.nonMaxSuppression = require('./lib/non-max-suppression');
 mAP.apScore = require('./lib/ap-score');
+mAP.matchPredictionsByLabelToAps = require('./lib/calculate-ap.js');
+mAP.getMatchPredictionsByLabel = require('./lib/get-match-predictions-by-label');
+mAP.reduceMatchPredictionsByLabel = require('./lib/reduce-match-predictions-by-label');
 
 module.exports = mAP;
